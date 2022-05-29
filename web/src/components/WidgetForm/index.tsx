54da23b3242c74bdcb1371c891e5030e8b08c6ab
Cezar Godoy <60990343+cezargodoy@users.noreply.github.com>
2022-05-29 02:52:43 -0300

    oi

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -63,7 +63,7 @@ export function WidgetForm() {
       ) }
 
       <footer className="text-xs text-neutral-400">
-        Feito por Cezar Godoy <a className="underline underline-offset-2" href="https://rocketseat.com.br">IFSP WEB2 Professor Johnata</a>
+        Feito por Cezar Godoy <a className="underline underline-offset-2" href="https://www.linkedin.com/in/cezargodoy/">IFSP WEB2 Professor Johnata</a>
       </footer>
     </div>
   )
