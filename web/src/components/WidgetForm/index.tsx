a6fea354c1a14c33609b90a3dba5f04a7b511a11
Cezar Godoy <60990343+cezargodoy@users.noreply.github.com>
2022-06-11 18:13:38 -0300

    teste

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -63,7 +63,7 @@ export function WidgetForm() {
       ) }
 
       <footer className="text-xs text-neutral-400">
-        Feito por Cezar Godoy <a className="underline underline-offset-2" href="https://www.linkedin.com/in/cezargodoy/">IFSP WEB2 Professor Johnata</a>
+        Feito com 💜 pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
       </footer>
     </div>
   )
---
+++ web/src/src/js/main.js
@@ -0,0 +1,61 @@
+const masks = {
+
+  
+   //Mascara para Data de Nome
+    name(value) {
+      return value;
+    },
+
+   //Mascara para Data de CPF
+    cpf(value) {
+      return value
+        .replace(/\D/g, '')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+        .replace(/(-\d{2})\d+?$/, '$1');
+    },
+
+
+   //Mascara para Data de Nascimento
+    date(value) {
+      return value
+    },
+
+
+   //Mascara para Email
+    email(value) {
+      return value
+    },
+
+
+   //Mascara para Telefone
+    fone(value) {
+      return value
+        .replace(/\D/g, '')
+        .replace(/(\d{2})(\d)/, '($1) $2')
+        .replace(/(\d{4})(\d)/, '$1-$2')
+        .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
+        .replace(/(-\d{4})\d+?$/, '$1');
+    },
+
+
+    //Mascara para CEP
+    cep(value) {
+      return value
+        .replace(/\D/g, '')
+        .replace(/(\d{5})(\d)/, '$1-$2')
+        .replace(/(-\d{3})\d+?$/, '$1');
+    },
+  };
+  
+  document.querySelectorAll('input').forEach(($input) => {
+    const field = $input.dataset.js;
+    $input.addEventListener(
+      'input',
+      (e) => {
+        e.target.value = masks[field](e.target.value);
+      },
+      false
+    );
+  });
